9990dee93f8dffab3da74fb83d4c46b3be5f8d1f
Nitish2773 <nitishkamisetti123@gmail.com>
2025-09-06 13:28:30 +0530

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
       <footer
         className="bg-gradient-to-r from-sriBlue-700 to-sriBlue-900
                    dark:from-gray-900 dark:to-gray-800
-                   text-white w-full z-40 md:fixed md:bottom-0 md:left-0 shadow-inner"
+                   w-full z-40 md:fixed md:bottom-0 md:left-0 shadow-inner"
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-0">
           
@@ -23,12 +23,12 @@ export default function Footer() {
           </div>
 
           {/* Footer message */}
-          <div className="text-sm italic text-center sm:text-center flex items-center justify-center gap-1 text-white dark:text-gray-200">
+          <div className="text-sm italic text-center sm:text-center flex items-center justify-center gap-1 text-black dark:text-white">
             Designed & Developed <FaHeart className="text-red-500" /> using MERN
           </div>
 
           {/* Copyright */}
-          <div className="text-sm text-center sm:text-right text-white dark:text-gray-200">
+          <div className="text-sm text-center sm:text-right text-black dark:text-white">
             © {new Date().getFullYear()} All rights reserved.
           </div>
         </div>
